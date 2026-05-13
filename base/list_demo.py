68707dd78c90c74d290321326926f5514f43180f
yangyang <yangy129@chinatelecom.cn>
2026-05-13 17:54:10 +0800

    tuple定义以及 list -> tuple

--- base/list_demo.py
+++ base/list_demo.py
@@ -12,5 +12,7 @@ if __name__ == "__main__":
     output_data = ["out1", "out2", "out3"]
     address = Address(input=input_data, output=output_data)
     print(address.input)
-    (in1, in2, in3) = address.input
+    (in1, in2, in3) = address.input  # list转元组
     print(in1)
+    in_first, *_ = address.input  # list转元组
+    print(in_first)
---
+++ base/tuple_demo.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    # 定义长度为3的元组
+    tup1: tuple[int, int, int] = (1, 2, 3)
+    tup2: tuple[int, int, str] = (1, 2, "3")
+    # 定义变长元组
+    tuple3: tuple[int, ...] = (1, 2, 3)
